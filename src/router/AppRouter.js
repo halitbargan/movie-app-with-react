baf2cd610fb7088e49c2023fc0e513b9c333950f
halitbargan <hltbrgn@gmail.com>
2022-04-11 22:45:11 +0300

    AppRouter completed

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,1 +1,21 @@
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import Main from "../pages/Main"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
 
+
+import React from 'react'
+
+const AppRouter = () => {
+  return (
+    <Router> 
+        <Routes> 
+            <Route path="/" element={<Main/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+        </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
