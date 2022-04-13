96d710a02807fa6de6661a2e3de1d80c4bdd785b
halitbargan <hltbrgn@gmail.com>
2022-04-13 22:57:28 +0300

    useContext is ok

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,51 +1,83 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  onAuthStateChanged
+} from "firebase/auth";
 
 //* https://firebase.google.com/docs/auth/web/start
-//* https://console.firebase.google.com/ => project setting
+//* https://console.firebase.google.com/ => project settings
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_apiKey,
+//   authDomain: process.env.REACT_APP_authDomain,
+//   projectId: process.env.REACT_APP_projectId,
+//   storageBucket: process.env.REACT_APP_storageBucket,
+//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//   appId: process.env.REACT_APP_appId,
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyBzEhg1WF7MLJNyLU_yl2IWuaA-6RcqjaY",
-    authDomain: "movie-app-13569.firebaseapp.com",
-    projectId: "movie-app-13569",
-    storageBucket: "movie-app-13569.appspot.com",
-    messagingSenderId: "766703358943",
-    appId: "1:766703358943:web:9b9b93cd9ae9379a302a6a",
-  };
+  apiKey: "AIzaSyBzEhg1WF7MLJNyLU_yl2IWuaA-6RcqjaY",
+  authDomain: "movie-app-13569.firebaseapp.com",
+  projectId: "movie-app-13569",
+  storageBucket: "movie-app-13569.appspot.com",
+  messagingSenderId: "766703358943",
+  appId: "1:766703358943:web:9b9b93cd9ae9379a302a6a",
+};
+
 // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-  const auth= getAuth(app);
+const auth = getAuth(app);
 
-  export const createUser = async (email, password,navigate) => {
-    try {
-      let userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      navigate("/")
-      console.log(userCredential);
-    } catch (err) {
-      alert(err.message);
-    }
-  };
-
-  export const signIn = async (email, password, navigate) => {
-    try {
-      let userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      navigate("/");
-      console.log(userCredential);
-    } catch (err) {
-      alert(err.message);
-    }
-  };
-
-  export const logOut=()=>{
-      signOut(auth)
-      alert("logged out successfully");
+export const createUser = async (email, password, displayName, navigate) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
   }
+};
+
+//* https://console.firebase.google.com/
+//* => Authentication => sign-in-method => enable Email/password
+export const signIn = async (email, password, navigate) => {
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export const logOut = () => {
+  signOut(auth);
+  alert("logged out successfully");
+};
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+      // ...
+    } else {
+      // User is signed out
+      setCurrentUser(false);
+    }
+  });
+};
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,1 +1,22 @@
+import {createContext} from "react"
+import {userObserver} from "../auth/firebase"
+import {useEffect,useState} from "react"
 
+export const AuthContext = createContext()
+
+const AuthContextProvider = ({children})=>{
+    const [currentUser, setCurrentUser] = useState()
+
+
+    useEffect(() => {
+      userObserver(setCurrentUser)
+    }, [])
+    
+    return (
+        <AuthContext.Provider value={{currentUser}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContextProvider
